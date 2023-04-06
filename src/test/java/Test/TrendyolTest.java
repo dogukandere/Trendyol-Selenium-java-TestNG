82cc787408248ef3901987344540b72a4a412aae
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2023-04-06 17:13:07 +0300

    Update TrendyolTest.java

--- src/test/java/Test/TrendyolTest.java
+++ src/test/java/Test/TrendyolTest.java
@@ -1,2 +1,33 @@
-package Test;public class trendyolTest {
+package Test;
+
+import Page.TrendyolPage;
+import Utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class TrendyolTest extends TestBase {
+    TrendyolPage trendyolPage;
+
+    @Test(priority = 1 , description = "Search product and add to cart")
+    public void test01(){
+        trendyolPage = new TrendyolPage(driver);
+        trendyolPage.navigateToUrl();
+        trendyolPage.closePopup();
+        trendyolPage.categoryHoverOver();
+        trendyolPage.logoTitleLinkControl();
+        trendyolPage.search("iphone 14");
+        trendyolPage.productClick();
+        trendyolPage.switchToTab();
+        trendyolPage.memorySelectandAddtoCart();
+        trendyolPage.buying();
+    }
+
+    @Test(priority = 2 , description = "Verify Product and Buttons")
+    public void test02(){
+        trendyolPage = new TrendyolPage(driver);
+        trendyolPage.isProductOnTheCart();
+        trendyolPage.isTrashButtonClickable();
+        trendyolPage.isDiscountButtonClickable();
+        trendyolPage.isShopButtonClickable();
+    }
 }
+
